f176216ea2110652bcb61c21f4f6c2e45e811863
siwaphon <gumert66@gmail.com>
2026-01-21 14:32:19 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,21 @@
 // Write your code below
+export{};
+
+const input = process.argv[2];
+const num = Number(input);
+
+if(isNaN(num)){
+    console.log("Invalid input")
+}else if ( num >= 80 && num <= 100){
+    console.log("A");
+}else if ( num >= 70 && num <= 79 ){
+    console.log("B");
+}else if ( num >= 60 && num <= 69 ){
+    console.log("C");
+}else if ( num >= 50 && num <= 59 ){
+    console.log("D");
+}else if ( num >= 0 && num <= 49 ){
+    console.log("F");
+}else{
+    console.log("Invalid input");
+}
