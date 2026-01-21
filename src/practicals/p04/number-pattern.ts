39bb3adffcd8c8e88459e54cc2999a30861ab0dd
siwaphon <gumert66@gmail.com>
2026-01-21 15:52:02 +0700

    p05

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,11 @@
 // Write your code below
+export{};
+
+const n = Number(process.argv[2]);
+for( let i=0; i<n; i++ ){
+  let row = ""
+  for(let j=0; j<n; j++ ){
+    row = "";
+  }
+  console.log(row)
+}
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,17 @@
+export{};
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
   // Write your code below
-}
+  const commentArray = comment.split(" ")
+  for(let i = 0; i < commentArray.length;i++ ){
+    if (commentArray[i].length >= 5 && commentArray[i].length <= 10){
+      console.log(commentArray[i]);
+    }else {
+      console.log("");
+    }
+  }
+  let result = commentArray.toString();
+  return result;
+} 
+
