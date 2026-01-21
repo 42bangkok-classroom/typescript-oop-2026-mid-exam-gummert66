dd14216471b02abadaeb74b522ef4f3f9f50b1df
siwaphon <gumert66@gmail.com>
2026-01-21 14:35:32 +0700

    p02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -7,15 +7,15 @@ const num = Number(input);
 if(isNaN(num)){
     console.log("Invalid input")
 }else if ( num >= 80 && num <= 100){
-    console.log("A");
+    console.log("Grade is A");
 }else if ( num >= 70 && num <= 79 ){
-    console.log("B");
+    console.log("Grade is B");
 }else if ( num >= 60 && num <= 69 ){
-    console.log("C");
+    console.log("Grade is C");
 }else if ( num >= 50 && num <= 59 ){
-    console.log("D");
+    console.log("Grade is D");
 }else if ( num >= 0 && num <= 49 ){
-    console.log("F");
+    console.log("Grade is F");
 }else{
     console.log("Invalid input");
 }
