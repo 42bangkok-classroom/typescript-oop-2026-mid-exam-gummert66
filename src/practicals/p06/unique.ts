bd59ecd5993b78cf7839db04fc15e5101461532d
siwaphon <gumert66@gmail.com>
2026-01-21 16:50:32 +0700

    p06

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -4,21 +4,24 @@ const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
 
 
-let i = 0;
+
 let arr3: number[] = [];
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
-  do{
-    if(arr1[i] === arr2[i]){
-      arr3 = [];
-    }else{
-      arr3.push(i)
+  for(let i = 0; i < arr1.length || arr2.length;i++){
+    for(let j = 0;j <arr1.length || arr2.length;j++ ){
+      if(arr1[i] === arr2[j]){
+        arr3 = [];
+      }else{
+        arr3.push(i);
+        break;
+      }
     }
-    i++;
-  }while (i < arr1.length || arr2.length);
-  return arr3
+  }
+ return arr3;
 }
 
 getUniqueNumbers(arr1,arr2);
+
 
 
