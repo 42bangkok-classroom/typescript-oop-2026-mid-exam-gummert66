911e13e4a0304235c1c32a1460472cc55d138843
siwaphon <gumert66@gmail.com>
2026-01-21 14:42:45 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,17 @@
 // Write your code below
+export{};
+
+const input = process.argv[2];
+const n = Number(input);
+
+for(let i = 1; i <= n;i++){
+    if( i%3 === 0 && i%5 === 0){
+        console.log("FizzBuzz");
+    }else if(i%3 === 0){
+        console.log("Fizz");
+    }else if(i%5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
